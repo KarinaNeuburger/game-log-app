9b872b0afb9392a27c892cb1097e7a23a2ec8977
KarinaNeuburger <nk37564@gmail.com>
2021-04-02 13:51:36 -0300

    search activity update

--- app/src/main/java/com/tep/gamelog/SearchActivity.java
+++ app/src/main/java/com/tep/gamelog/SearchActivity.java
@@ -18,6 +18,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import android.widget.SearchView;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -37,7 +38,9 @@ public class SearchActivity extends AppCompatActivity {
 
     private EditText searchText;
     private TextView resultText;
-    private Button searchButton;
+    private SearchView searchView;
+    public String queryText;
+    public Toast toast;
 
     private GameSQLite gameatual = null;
     private GameDAO dao;
@@ -45,85 +48,60 @@ public class SearchActivity extends AppCompatActivity {
     private boolean contemGame = false;
     private List<Game> gamelist;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
+        FloatingActionButton addButton = findViewById(R.id.add_button);
+        FloatingActionButton backButton = findViewById(R.id.back_button);
         searchText = findViewById(R.id.search_text);
         resultText = findViewById(R.id.search_result_text);
-        searchButton = findViewById(R.id.search_button);
+        searchView = findViewById(R.id.search_view);
+        searchView.setSubmitButtonEnabled(true);
+        searchView.setIconifiedByDefault(false);
+        addButton.setEnabled(false);
 
         final DBUtil dbutil = DBUtil.getInstance(this.getApplicationContext());
         dao = new GameDAO(dbutil.getDb());
         games = dao.lista();
 
-        searchButton.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public void onClick(View view) {
+            public boolean onQueryTextSubmit(String query) {
+                Log.d("Query", "QUERY DE PESQUISA" + query);
 
-                ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE))
-                        .hideSoftInputFromWindow(searchText.getWindowToken(), 0);
+                queryText = query.toUpperCase();
 
-                //final ProgressDialog progressDialog;
-                //progressDialog = new ProgressDialog(SearchActivity.this);
-                //progressDialog.setMax(100);
-                //progressDialog.setMessage("Searching...");
-                //progressDialog.setTitle("Game Log API v 1.0");
-                //progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-                //progressDialog.show();
-                
+                Log.d("Query up", "QUERY TO UPPER" + queryText);
+
                 AlertDialog.Builder builder = new AlertDialog.Builder(SearchActivity.this);
                 builder.setCancelable(false); // if you want user to wait for some process to finish,
                 builder.setView(R.layout.dialog_loading);
                 builder.setTitle("Game Log API v 1.0");
                 AlertDialog dialog = builder.create();
-                
+
                 dialog.show(); // to show this dialog
-                
-                Log.d("Teste", "Teste");
 
                 GameService service = RetrofitConfig.getRetrofitInstance().create(GameService.class);
-                Call<List<Game>> call = service.findGameTitle(searchText.getText().toString());
+                Call<List<Game>> call = service.findGameTitle(queryText);
                 call.enqueue(new Callback<List<Game>>() {
-                //Call<List<Game>> call = new RetrofitConfig().getGameService().listGames();
-                //call.enqueue(new Callback<List<Game>>() {
+                    //Call<List<Game>> call = new RetrofitConfig().getGameService().listGames();
+                    //call.enqueue(new Callback<List<Game>>() {
                     @Override
                     public void onResponse(Call<List<Game>> call, Response<List<Game>> response) {
                         Log.d("onResponse", "Entrou no onResponse");
-                        //progressDialog.dismiss();
-                        /*new Thread(new Runnable() {
-                            @Override
-                            public void run() {
-                                try {
-                                    Thread.sleep(2000);
-                                    Thread.wait();
-                                    dialog.dismiss();
-                                }
-                                catch(InterruptedException ex){
-                                    ex.printStackTrace();
-                                }
-                            }
-                        }).start();*/
                         dialog.dismiss(); // to hide this dialog
                         gamelist = response.body();
                         Log.d("Teste", "GameList" + gamelist.toString());
                         if (!gamelist.isEmpty()) {
 
-                            //Intent intent = new Intent(SearchActivity.this, FilmInfo.class);
-                            Bundle parametros = new Bundle();
-
-                            parametros.putString("id", gamelist.get(0).getId());
-                            parametros.putString("titulo", gamelist.get(0).getTitle());
-                            parametros.putInt("lançamento", gamelist.get(0).getRelease());
+                            addButton.setEnabled(true);
 
                             resultText.setText(gamelist.get(0).getId() +
                                     "\n" + gamelist.get(0).getTitle() +
                                     "\n" + gamelist.get(0).getRelease());
-
-                            //intent.putExtras(parametros);
-                            //startActivity(intent);
 
                         }else{
                             resultText.setText("Nenhum jogo encontrado");
@@ -132,16 +110,24 @@ public class SearchActivity extends AppCompatActivity {
 
                     @Override
                     public void onFailure(Call<List<Game>> call, Throwable t) {
-                        //progressDialog.dismiss();
+
                         dialog.dismiss(); // to hide this dialog
                         resultText.setText("Erro ao buscar dados na API");
                         Log.e("GameService   ", "Erro ao buscar dados na API:" + t.getMessage());
                     }
                 });
+
+                return false;
             }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //Log.d("Query", "QUERY DE PESQUISA ON TEXT CHANGE" + newText);
+                return false;
+            }
+
         });
 
-        FloatingActionButton addButton = findViewById(R.id.add_button);
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -164,7 +150,7 @@ public class SearchActivity extends AppCompatActivity {
                             //dao.alterar(gameatual);
                             contemGame = true;
                             Log.d("ER", "alterar");
-                            //Toast.makeText(SearchActivity.this, "Registro alterado com sucesso", Toast.LENGTH_SHORT).show();
+                            toast.makeText(SearchActivity.this, "Registro alterado com sucesso", Toast.LENGTH_SHORT).show();
                         }
                     }
                 }
@@ -180,7 +166,6 @@ public class SearchActivity extends AppCompatActivity {
             }
         });
 
-        FloatingActionButton backButton = findViewById(R.id.back_button);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
